b5785f1d66b9e1a15eaf80560c0e3d882de95212
shubh24 <shubhankar24@gmail.com>
2016-05-08 23:35:52 +0530

    Input standardize + plotting

--- explore.R
+++ explore.R
@@ -1,1 +1,56 @@
-data = read.csv("../data.csv", stringsAsFactors = FALSE)
+source('./take_input.R')
+library(ggplot2)
+library(dplyr)
+
+# Plotting combined shot type w.r.t lat and lon
+ggplot() + 
+geom_point(data = subset(train, train$combined_shot_type == "Jump Shot"), aes(x = loc_x, y = loc_y), color = "grey", alpha = 0.3, size = 2) + 
+geom_point(data = subset(train, train$combined_shot_type != "Jump Shot"), aes(x = loc_x, y = loc_y, color = combined_shot_type), alpha = 0.7, size = 3)+
+ylim(c(-50, 300))+
+scale_color_brewer(palette = "Set1") +
+ggtitle("Shot Types")
+
+# Plotting shot made flag w.r.t lat and lon
+ggplot() + 
+geom_point(data = subset(train, train$combined_shot_type != "Jump Shot"), aes(x = loc_x, y = loc_y, color = shot_made_flag), alpha = 0.3, size = 2) + 
+ylim(c(-30, 50))+
+xlim(c(-100,100))+
+scale_color_brewer(palette = "Set1") +
+ggtitle("Shot Made w.r.t Position")
+
+# Plotting various parameters on the court. 
+# Pro Tip : aes_q used for aesthetics on string variables.
+courtplot <- function(feat) {
+  feat <- substitute(feat)
+  ggplot(data = train, aes(x = loc_x, y = loc_y)) +
+  geom_point(aes_q(color = feat), alpha = 0.7, size = 3) +
+  ylim(c(-50, 300)) +
+  scale_color_brewer(palette = "Set1") +
+  ggtitle(paste(feat))
+}
+
+courtplot(shot_zone_area)
+courtplot(shot_zone_basic)
+courtplot(shot_zone_range)
+courtplot(shot_made_flag)
+
+#A plot to see accuracy by feature
+pplot <- function(feat) {
+  feat <- substitute(feat)
+  ggplot(data = train, aes_q(x = feat)) +
+  geom_bar(aes(fill = shot_made_flag), stat = "count", position = "fill") +
+  scale_fill_brewer(palette = "Set1") +
+  ggtitle(paste("Shots by", feat))
+}
+
+pplot(minutes_remaining)
+pplot(seconds_remaining)
+pplot(period)
+pplot(opponent)
+pplot(season)
+pplot(shot_distance)
+pplot(matchup)
+pplot(combined_shot_type) + coord_flip()
+pplot(shot_zone_area) + coord_flip()
+pplot(shot_zone_range) + coord_flip()
+pplot(shot_zone_basic) + coord_flip()
---
+++ take_input.R
@@ -0,0 +1,25 @@
+library(dplyr)
+data = read.csv("../data.csv", stringsAsFactors = FALSE)
+data$team_name = NULL
+data$team_id = NULL
+data$playoffs = NULL #Doesn't contribute to shot prediction
+data$lat = NULL
+data$lon = NULL
+
+data[grep("@", data$matchup),"matchup"] = "AWAY"
+data[grep("vs.", data$matchup),"matchup"] = "HOME"
+data$action_type = as.factor(data$action_type)
+data$combined_shot_type = as.factor(data$combined_shot_type)
+data$period = as.factor(data$period)
+data$season = as.factor(data$season)
+data$shot_type = as.factor(data$shot_type)
+data$shot_zone_area = as.factor(data$shot_zone_area)
+data$shot_zone_basic = as.factor(data$shot_zone_basic)
+data$shot_zone_range = as.factor(data$shot_zone_range)
+data$opponent = as.factor(data$opponent)
+data$game_date = as.Date(data$game_date)
+
+train = subset(data, !is.na(data$shot_made_flag))
+test = subset(data, is.na(data$shot_made_flag))
+
+train$shot_made_flag <- as.factor(train$shot_made_flag)
