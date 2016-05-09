b94c1a0221b142bc5844606258d4c212b2eb05aa
shubh24 <shubhankar24@gmail.com>
2016-05-09 16:01:19 +0530

    Train on previous seasons + euclidean distance + angle

--- tree_forest.R
+++ tree_forest.R
@@ -10,10 +10,10 @@ data$game_id = NULL
 data$seconds_remaining = NULL
 data$shot_distance = NULL #Banking on shot_zone_range for this
 data$game_date = NULL
-#data$shot_id = NULL
-#data$action_type = NULL #RF can't handle more than 53 categories
-data$loc_x = NULL #Experiment
-data$loc_y = NULL #Experiment
+data$distance = (data$loc_x^2 + data$loc_y^2)^(1/2)  
+data$angle = atan(data$loc_y/data$loc_x) #angles in radians
+data$loc_x = NULL 
+data$loc_y = NULL 
 
 train = subset(data, !is.na(data$shot_made_flag))
 test = subset(data, is.na(data$shot_made_flag))
@@ -25,18 +25,22 @@ result = data.frame(nrow = nrow(test), ncol = 2)
 colnames(result) = c("shot_id", "shot_made_flag")
 
 for(i in levels(train$season)){
-  train_season = subset(train, train$season == i)
-  test_season = subset(test, test$season == i)
+  train_season = subset(train, as.character(train$season) <= i)
+  test_season = subset(test, as.character(test$season) == i)
   train_season$season = NULL
   test_season$season = NULL
   
-  #rf = randomForest(shot_made_flag ~ . , data = train_season)
-  #pred_rf = predict(rf, test_season)
-  tree = ctree(shot_made_flag ~ . - shot_id, data = train_season)
+
+  tree = ctree(shot_made_flag~.-shot_id, data = train_season)
   pred_tree = predict(tree,  type = "prob", test_season)
   
   for (j in 1:length(pred_tree)){
     result = rbind(result, list(test_season[j,]$shot_id, pred_tree[j][[1]][2]))
   }
+  
+  #rf = randomForest(shot_made_flag ~ . , data = train_season)
+  #pred_rf = predict(rf, test_season)
   #xgb = xgboost(data = as.matrix(train_season), label = as.matrix(as.integer(train_season$shot_made_flag)), max.depth = 2,eta = 1, nthread = 2, nround = 2,objective = "binary:logistic")
-  }
+}
+
+write.csv(result, "cTree_Train_Previous_Seasons_with_Distance.csv")
